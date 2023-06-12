378a924f96381af9a7c188aed90ad71f9ccba0ab
Alex Sheiko <kelamir@protonmail.com>
2023-06-12 23:56:31 +0300

    Clean up comments

--- main.go
+++ main.go
@@ -9,21 +9,14 @@ import (
 	"strings"
 )
 
-// Remove HTML tags using regular expressions
 func stripHTMLTags(text string) string {
 	re := regexp.MustCompile("<[^>]*>")
 	return re.ReplaceAllString(text, "")
 }
 
-// Remove Markdown formatting
 func stripMarkdown(text string) string {
-	// Remove bold formatting (e.g., **bold**)
 	text = strings.ReplaceAll(text, "**", "")
-
-	// Remove italic formatting (e.g., *italic*)
 	text = strings.ReplaceAll(text, "*", "")
-
-	// Remove headers (e.g., ## Header)
 	re := regexp.MustCompile(`(?m)^#+\s*`)
 	text = re.ReplaceAllString(text, "")
 
@@ -31,7 +24,6 @@ func stripMarkdown(text string) string {
 }
 
 func main() {
-	// Open the input CSV file
 	csvFile, err := os.Open("output.csv")
 	if err != nil {
 		fmt.Println("Error opening CSV file:", err)
@@ -39,7 +31,6 @@ func main() {
 	}
 	defer csvFile.Close()
 
-	// Create the output CSV file
 	outputFile, err := os.Create("outputfinal.csv")
 	if err != nil {
 		fmt.Println("Error creating output file:", err)
@@ -47,13 +38,10 @@ func main() {
 	}
 	defer outputFile.Close()
 
-	// Create a CSV reader
 	reader := csv.NewReader(csvFile)
 
-	// Create a CSV writer
 	writer := csv.NewWriter(outputFile)
 
-	// Read and write CSV records
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
@@ -64,11 +52,9 @@ func main() {
 			return
 		}
 
-		// Strip HTML and Markdown formatting from the second column
 		record[1] = stripHTMLTags(record[1])
 		record[1] = stripMarkdown(record[1])
 
-		// Write the modified record to the output CSV file
 		err = writer.Write(record)
 		if err != nil {
 			fmt.Println("Error writing CSV record:", err)
@@ -76,7 +62,6 @@ func main() {
 		}
 	}
 
-	// Flush any buffered content to the output file
 	writer.Flush()
 
 	fmt.Println("CSV processing completed.")
