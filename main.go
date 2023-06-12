f3cd6a0d2d4a0e7f2c46ee909f7f91c03cd2a201
Alex Sheiko <kelamir@protonmail.com>
2023-06-12 23:54:00 +0300

    Updated

--- main.go
+++ main.go
@@ -1,63 +1,83 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
-	"log"
+	"fmt"
+	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
+// Remove HTML tags using regular expressions
+func stripHTMLTags(text string) string {
+	re := regexp.MustCompile("<[^>]*>")
+	return re.ReplaceAllString(text, "")
+}
+
+// Remove Markdown formatting
+func stripMarkdown(text string) string {
+	// Remove bold formatting (e.g., **bold**)
+	text = strings.ReplaceAll(text, "**", "")
+
+	// Remove italic formatting (e.g., *italic*)
+	text = strings.ReplaceAll(text, "*", "")
+
+	// Remove headers (e.g., ## Header)
+	re := regexp.MustCompile(`(?m)^#+\s*`)
+	text = re.ReplaceAllString(text, "")
+
+	return text
+}
+
 func main() {
-	file, err := os.Open("input.md")
+	// Open the input CSV file
+	csvFile, err := os.Open("output.csv")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error opening CSV file:", err)
+		return
 	}
-	defer file.Close()
-
-	// Open CSV writer
-	csvFile, err := os.Create("output.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	csvWriter := csv.NewWriter(csvFile)
-
-	defer csvWriter.Flush()
 	defer csvFile.Close()
 
-	scanner := bufio.NewScanner(file)
-	var question string
-	var answer strings.Builder
+	// Create the output CSV file
+	outputFile, err := os.Create("outputfinal.csv")
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		return
+	}
+	defer outputFile.Close()
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	// Create a CSV reader
+	reader := csv.NewReader(csvFile)
 
-		// check if the line contains "###"
-		if strings.Contains(line, "###") {
-			if question != "" {
-				// Write the previous question and answer to the csv file
-				csvWriter.Write([]string{question, answer.String()})
-				answer.Reset()
-			}
-			// Get the question from the line, without the "###"
-			question = strings.TrimSpace(strings.Split(line, "###")[1])
-		} else {
-			// Add the line to the current answer
-			answer.WriteString(line + "\n")
+	// Create a CSV writer
+	writer := csv.NewWriter(outputFile)
+
+	// Read and write CSV records
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading CSV record:", err)
+			return
+		}
+
+		// Strip HTML and Markdown formatting from the second column
+		record[1] = stripHTMLTags(record[1])
+		record[1] = stripMarkdown(record[1])
+
+		// Write the modified record to the output CSV file
+		err = writer.Write(record)
+		if err != nil {
+			fmt.Println("Error writing CSV record:", err)
+			return
 		}
 	}
 
-	// Check for any scanner error
-	if err = scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// Flush any buffered content to the output file
+	writer.Flush()
 
-	// Write the last question and answer to the csv file
-	if question != "" {
-		csvWriter.Write([]string{question, answer.String()})
-	}
-
-	if err := csvWriter.Error(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("CSV processing completed.")
 }
